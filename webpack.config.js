fe7db95409996e54173e0765321588fb495e9339
Jim Wright <jim@anow.com>
2015-11-05 21:50:24 -0700

    removing deps from bundle

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: __dirname,
-    filename: "./dist/bundle.js"
+    filename: "./dist/bundle.js",
+    libraryTarget: "umd"
   },
   module: {
     loaders: [{
@@ -10,5 +11,9 @@ module.exports = {
       loader: 'babel?optional[]=runtime',
       exclude: /node_modules/
     }]
-  }
+  },
+  externals: [
+    'angular',
+    'angular-formly'
+  ]
 };
